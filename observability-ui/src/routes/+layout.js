5d56ec20fa4032a793f9ed95a87c04133c2c96d7
Conductor Coder <coder@conductor.local>
2026-04-16 20:27:16 +0000

    chore: remove accidental build artifacts from src

--- observability-ui/src/routes/+layout.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.trailingSlash = exports.prerender = void 0;
-exports.prerender = true;
-exports.trailingSlash = 'always';
